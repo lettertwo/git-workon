3d4b62d99f425d2a91556578889112d8c38378bf
Eric Eldredge <lettertwo@gmail.com>
2022-10-25 15:55:46 -0400

    Extract commands from main and define a Run trait

---
+++ src/cmd.rs
@@ -0,0 +1,135 @@
+use anyhow::{bail, Ok, Result};
+use clap::{Args, Subcommand};
+use log::{debug, info, trace};
+
+// use std::ffi::OsStr;
+// use std::ffi::OsString;
+use std::path::PathBuf;
+
+pub trait Run {
+    fn run(&self) -> Result<()>;
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Cmd {
+    Clone(Clone),
+    CopyUntracked(CopyUntracked),
+    Init(Init),
+    List(List),
+    New(New),
+    Prune(Prune),
+    Worktree(Worktree),
+}
+
+impl Cmd {
+    pub fn run(&self) -> Result<()> {
+        match self {
+            Cmd::Clone(cmd) => cmd.run(),
+            Cmd::CopyUntracked(cmd) => cmd.run(),
+            Cmd::Init(cmd) => cmd.run(),
+            Cmd::List(cmd) => cmd.run(),
+            Cmd::New(cmd) => cmd.run(),
+            Cmd::Prune(cmd) => cmd.run(),
+            Cmd::Worktree(cmd) => cmd.run(),
+        }
+    }
+}
+
+/// Perform a bare clone of a repository and create an initial worktree.
+#[derive(Debug, Args)]
+pub struct Clone {
+    pub url: String,
+}
+
+impl Run for Clone {
+    fn run(&self) -> Result<()> {
+        bail!("Clone Not implemented");
+    }
+}
+
+/// Copy any untracked files in <from> to <to>.
+///
+/// Untracked files are files that are ignored by git, or files that are not in the git index.
+///
+/// This util is a useful complement to a git worktree workflow. Git worktrees provide
+/// a mechanism for maintaining multiple branches of a repository simultaneously, without
+/// having to switch between branches (and using stash to keep WIP stuff around, etc).
+/// See `man git-worktree` for more information.
+///
+/// However, one drawback of this approach vs. the traditional branch workflow is that any untracked
+/// artifacts in the working directory, such as installed node modules, build caches, etc.,
+/// have to be recreated or otherwise manually copied over when creating a new worktree.
+///
+/// That's where `copyuntracked` comes in!
+///
+/// If possible, copying will be done using `clonefile` (`man clonefile`),
+/// which is a copy-on-write optimization over a potentially much slower copy operation.
+#[derive(Debug, Args)]
+pub struct CopyUntracked {
+    pub from: String,
+    pub to: String,
+}
+
+impl Run for CopyUntracked {
+    fn run(&self) -> Result<()> {
+        bail!("copyuntracked from={} to={} not implemented!", self.from, self.to);
+    }
+}
+
+/// Create a new bare repository and an initial worktree.
+#[derive(Debug, Args)]
+pub struct Init {
+    pub name: Option<PathBuf>,
+}
+
+impl Run for Init {
+    fn run(&self) -> Result<()> {
+        bail!("init name={:?} not implemented!", self.name);
+    }
+}
+
+/// List worktrees.
+#[derive(Debug, Args)]
+pub struct List {}
+
+impl Run for List {
+    fn run(&self) -> Result<()> {
+        bail!("list not implemented!");
+    }
+}
+
+/// Create a new worktree.
+#[derive(Debug, Args)]
+pub struct New {
+    pub name: Option<String>,
+}
+
+impl Run for New {
+    fn run(&self) -> Result<()> {
+        bail!("new name={:?} not implemented!", self.name);
+    }
+}
+
+/// Prune stale worktrees.
+#[derive(Debug, Args)]
+pub struct Prune {}
+
+impl Run for Prune {
+    fn run(&self) -> Result<()> {
+        bail!("prune not implemented!");
+    }
+}
+
+/// Select a worktree to work on.
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+pub struct Worktree {
+    /// The name of the worktree to work on.
+    pub name: Option<String>,
+}
+
+impl Run for Worktree {
+    fn run(&self) -> Result<()> {
+        bail!("worktree name={:?} not implemented!", self.name);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,54 @@
-use anyhow::{bail, Ok, Result};
-use clap::{Args, Parser, Subcommand};
-use clap_verbosity_flag::{InfoLevel, Verbosity};
-use log::{debug, info};
+mod cmd;
 
-// use std::ffi::OsStr;
-// use std::ffi::OsString;
-use std::path::PathBuf;
+use std::process::ExitCode;
+
+use clap::Parser;
+use clap_verbosity_flag::{InfoLevel, Verbosity};
+use log::error;
+
+use crate::cmd::{Cmd, Worktree};
 
 #[derive(Debug, Parser)]
-#[command(author, version, about, long_about = None)]
-#[command(propagate_version = true)]
+#[clap(
+    about,
+    author,
+    bin_name = env!("CARGO_PKG_NAME"),
+    disable_help_subcommand = true,
+    propagate_version = true,
+    version,
+)]
+
 struct Cli {
     #[clap(flatten)]
-    verbose: Verbosity<InfoLevel>,
+    pub verbose: Verbosity<InfoLevel>,
     #[command(subcommand)]
-    command: Option<Commands>,
+    pub command: Option<Cmd>,
     #[clap(flatten)]
-    worktree: Worktree,
+    pub worktree: Worktree,
 }
 
-#[derive(Debug, Args)]
-#[command(args_conflicts_with_subcommands = true)]
-struct Worktree {
-    /// The name of the worktree to work on.
-    name: Option<String>,
-}
-
-// TODO: Default subcommand? https://github.com/clap-rs/clap/issues/975#issuecomment-1012701551
-#[derive(Debug, Subcommand)]
-enum Commands {
-    /// Create a new bare repository and an initial worktree.
-    Init {
-        name: Option<PathBuf>,
-    },
-    /// Select a worktree to work on.
-    Worktree(Worktree),
-
-    /// List worktrees.
-    List {},
-
-    /// Create a new worktree.
-    New {
-        name: Option<String>,
-    },
-
-    /// Prune stale worktrees.
-    Prune {},
-
-    ///
-    /// Copy any untracked files in <from> to <to>.
-    ///
-    /// Untracked files are files that are ignored by git, or files that are not in the git index.
-    ///
-    /// This util is a useful complement to a git worktree workflow. Git worktrees provide
-    /// a mechanism for maintaining multiple branches of a repository simultaneously, without
-    /// having to switch between branches (and using stash to keep WIP stuff around, etc).
-    /// See `man git-worktree` for more information.
-    ///
-    /// However, one drawback of this approach vs. the traditional branch workflow is that any untracked
-    /// artifacts in the working directory, such as installed node modules, build caches, etc.,
-    /// have to be recreated or otherwise manually copied over when creating a new worktree.
-    ///
-    /// That's where `copyuntracked` comes in!
-    ///
-    /// If possible, copying will be done using `clonefile` (`man clonefile`),
-    /// which is a copy-on-write optimization over a potentially much slower copy operation.
-    CopyUntracked {
-        from: String,
-        to: String,
-    },
-}
-
-fn main() -> Result<()> {
+fn main() -> ExitCode {
     let cli = Cli::parse();
 
     env_logger::Builder::new()
         .filter_level(cli.verbose.log_level_filter())
         .init();
 
-    debug!("This is a debug message");
-
-    match &cli.command {
-        Some(Commands::Init { name }) => {
-            info!("Initializing worktree {:#?}", name);
-        }
-        Some(Commands::List {}) => {
-            info!("Listing worktrees");
-        }
-        Some(Commands::New { name }) => {
-            info!("Creating new worktree: {:?}", name);
-        }
-        Some(Commands::Prune {}) => {
-            info!("Pruning stale worktrees");
-        }
-        Some(Commands::Worktree(name)) => {
-            info!("Worktree {:?}", name);
-        }
-        Some(Commands::CopyUntracked { from, to }) => {
-            info!("CopyUntracked {:?} -> {:?}", from, to);
-        }
+    match cli.command {
+        Some(cmd) => match cmd.run() {
+            Ok(()) => ExitCode::SUCCESS,
+            Err(e) => {
+                error!("{}", e);
+                eprintln!("{}", e);
+                ExitCode::FAILURE
+            }
+        },
         None => {
-            bail!("No subcommand was used. Try --help for more information.")
+            eprintln!(
+                "Invalid subcommand {:?}. Try --help for more information.",
+                cli.command
+            );
+            ExitCode::FAILURE
         }
     }
-
-    Ok(())
 }
