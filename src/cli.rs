16ad8998c571e81e541533ee7e7b3ca3e409676d
Eric Eldredge <lettertwo@gmail.com>
2022-10-29 12:01:41 -0400

    Add clap debug_assert test

--- src/cli.rs
+++ src/cli.rs
@@ -89,3 +89,14 @@ pub struct Switch {
     /// The name of the worktree to work on.
     pub name: Option<String>,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use clap::CommandFactory;
+
+    #[test]
+    fn verify_cli() {
+        Cli::command().debug_assert()
+    }
+}
