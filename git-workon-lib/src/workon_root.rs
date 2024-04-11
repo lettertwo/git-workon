bba7a966a519da8fcb3fd6064b5cbcc43352e891
Eric Eldredge <lettertwo@gmail.com>
2024-04-11 11:00:31 -0400

    Fix workon_root for existing worktrees

--- git-workon-lib/src/workon_root.rs
+++ git-workon-lib/src/workon_root.rs
@@ -4,5 +4,20 @@ use git2::Repository;
 use miette::Result;
 
 pub fn workon_root(repo: &Repository) -> Result<&Path> {
-    Ok(repo.path().parent().unwrap())
+    let path = repo.path();
+
+    match repo.workdir() {
+        Some(workdir) if workdir != path => {
+            let repo_ancestors: Vec<_> = path.ancestors().collect();
+            let common_root = workdir
+                .ancestors()
+                .find(|ancestor| repo_ancestors.contains(ancestor));
+            if let Some(common_root) = common_root {
+                return Ok(common_root);
+            }
+        }
+        _ => {}
+    }
+
+    Ok(path.parent().unwrap())
 }
