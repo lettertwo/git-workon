170f46f2b3a69ff581a5c5ff62a6ae4eaa971de4
Eric Eldredge <lettertwo@gmail.com>
2024-04-11 19:07:50 -0400

    Capture some notes about unimplemented features

--- git-workon/src/cmd/prune.rs
+++ git-workon/src/cmd/prune.rs
@@ -4,6 +4,16 @@ use crate::cli::Prune;
 
 use super::Run;
 
+// Cleanup:
+//
+//   `git worktree remove ../lettertwo/some-thing \
+//     && git branch -d lettertwo/some-thing`
+//
+// Cleanup remote:
+//
+//   `git worktree remove ../bdo/browser-reporter \
+//     && git branch -d bdo/browser-reporter \
+//     && git push --delete origin bdo/browser-reporter`
 
 impl Run for Prune {
     fn run(&self) -> Result<()> {
