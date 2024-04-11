a1fb07864c063497e40962d6d7e8271f823ebe26
Eric Eldredge <lettertwo@gmail.com>
2024-04-11 11:09:21 -0400

    Add basic new cmd impl

--- git-workon-lib/src/add_worktree.rs
+++ git-workon-lib/src/add_worktree.rs
@@ -1,20 +1,41 @@
 use git2::{Repository, Worktree, WorktreeAddOptions};
 use log::debug;
-use miette::{IntoDiagnostic, Result};
+use miette::{bail, IntoDiagnostic, Result};
 
 use super::workon_root;
 
-pub fn add_worktree(repo: &Repository, branch: &str) -> Result<Worktree> {
+pub fn add_worktree(repo: &Repository, branch_name: &str) -> Result<Worktree> {
     // git worktree add <branch>
-    debug!("Adding worktree {}", branch);
-    let reference = repo
-        .find_branch(branch, git2::BranchType::Local)
-        .into_diagnostic()?
-        .into_reference();
+    debug!("looking for local branch {:?}", branch_name);
+
+    let branch = repo
+        .find_branch(branch_name, git2::BranchType::Local)
+        .into_diagnostic()
+        .or_else(|e| {
+            debug!("local branch not found: {:?}", e);
+            debug!("looking for remote branch {:?}", branch_name);
+            repo.find_branch(branch_name, git2::BranchType::Remote)
+                .into_diagnostic()
+                .or_else(|e| {
+                    debug!("remote branch not found: {:?}", e);
+                    Err(e)
+                })
+        })
+        .ok()
+        .unwrap_or_else(|| {
+            debug!("creating new local branch {:?}", branch_name);
+            let commit = repo.head().unwrap().peel_to_commit().unwrap();
+            repo.branch(branch_name, &commit, false)
+                .into_diagnostic()
+                .unwrap()
+        });
+
+    let reference = branch.into_reference();
+
     let root = workon_root(repo)?;
     let mut opts = WorktreeAddOptions::new();
     opts.reference(Some(&reference));
-    Ok(repo
-        .worktree(branch, root.join(branch).as_path(), Some(&opts))
-        .into_diagnostic()?)
+    debug!("adding worktree at {}", root.join(branch_name).display());
+    repo.worktree(branch_name, root.join(branch_name).as_path(), Some(&opts))
+        .into_diagnostic()
 }
--- git-workon/src/cmd/new.rs
+++ git-workon/src/cmd/new.rs
@@ -1,11 +1,51 @@
 use miette::{bail, Result};
 
 use crate::cli::New;
+use workon::{add_worktree, get_repo};
 
 use super::Run;
 
+// Ability to easily create a worktree with namespcaing.
+// Also see: https://lists.mcs.anl.gov/pipermail/petsc-dev/2021-May/027436.html
+//
+// The anatomy of the command is:
+//
+//   `git worktree add --track -b <branch> ../<path> <remote>/<remote-branch>`
+//
+// we want `<branch>` to exactly match `<remote-branch>`
+// We want `<path>` to exactly match `<branch>`
+//
+// Use case: checking out an existing branch
+//
+//   `git worktree add --track -b bdo/browser-reporter ../bdo/browser-reporter origin/bdo/browser-reporter`
+//
+// Use case: creating a new branch
+// In this case, we aren't tracking a remote (yet?)
+//
+//   `git worktree add -b lettertwo/some-thing ../lettertwo/some-thing`
+//
+// Hooks: on creation, we will often want to copy artifacts from the base worktree (e.g., node_modules, build dirs)
+// One approach to this is the `copyuntracked` util that can (perhaps interactively?) copy over
+// any untracked or git ignored files. It would be nice if this script was also SCM-aware, in that it could
+// suggest rebuilds, or re-running install, etc, if the base artifacts are much older than the new worktree HEAD.
+
 impl Run for New {
     fn run(&self) -> Result<()> {
-        bail!("new name={:?} not implemented!", self.name);
+        let name = match &self.name {
+            Some(name) => name,
+            None => {
+                bail!("Interactive new not implemented!");
+            }
+        };
+        let repo = get_repo(None)?;
+
+        match add_worktree(&repo, &name) {
+            Ok(worktree) => {
+                println!("Created worktree at: {}", worktree.path().display());
+            }
+            Err(e) => bail!(e),
+        };
+
+        Ok(())
     }
 }
