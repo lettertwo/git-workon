18d0a999c4422d63ab3aa8abeefe72cb165b0e5e
Eric Eldredge <lettertwo@gmail.com>
2025-12-18 14:25:08 -0500

    Fix lifetime bounds in git-workon-fixture assert trait

--- git-workon-fixture/src/assert.rs
+++ git-workon-fixture/src/assert.rs
@@ -6,7 +6,11 @@ pub trait FixtureAssert {
     fn assert<I, P>(&self, predicate: I) -> &Self
     where
         I: IntoFixturePredicate<P, Repository>,
-        P: Predicate<Repository>;
+        P: Predicate<Repository> + 'static;
+}
+
+pub trait DescribesActual {
+    fn describe_actual(&self, repo: &git2::Repository) -> Option<String>;
 }
 
 impl FixtureAssert for Repository {
@@ -14,16 +18,33 @@ impl FixtureAssert for Repository {
     fn assert<I, P>(&self, predicate: I) -> &Self
     where
         I: IntoFixturePredicate<P, Repository>,
-        P: Predicate<Repository>,
+        P: Predicate<Repository> + 'static,
     {
         let pred = predicate.into();
         let repo = self;
-        assert!(
-            pred.eval(repo),
-            "Repository assertion failed.\nPredicate: {}\nRepo: {:?}",
-            pred,
-            repo.path()
-        );
+
+        let extra = match (&pred as &dyn std::any::Any).downcast_ref::<&dyn DescribesActual>() {
+            Some(describer) => describer.describe_actual(repo),
+            None => None,
+        };
+
+        if let Some(extra) = extra {
+            assert!(
+                pred.eval(repo),
+                "Repository assertion failed.\nPredicate: {}\nActual: {}\nRepo: {:?}",
+                pred,
+                extra,
+                repo.path(),
+            );
+        } else {
+            assert!(
+                pred.eval(repo),
+                "Repository assertion failed.\nPredicate: {}\nRepo: {:?}",
+                pred,
+                repo.path(),
+            );
+        }
+
         self
     }
 }
